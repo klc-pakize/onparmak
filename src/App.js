4b9538cb76b83b4d9d4761ca40ae4ea74e36efd4
klc-pakize <pa3kize@gmail.com>
2023-02-18 18:26:44 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import Main from "./components/Main";
+import Textarea from "./components/Textarea";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Main />
+      <Textarea />
+    </div>
+  );
+}
+
+export default App;
